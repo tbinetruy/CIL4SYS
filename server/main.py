a7fcf4350b2d8a704ad0ff6d92bb04421ba1d12b
Thomas Binetruy <tbinetruy@gmail.com>
2019-03-28 00:48:54 +0100

    added tensorflow graph calc eg to post view

--- server/main.py
+++ server/main.py
@@ -10,9 +10,7 @@ def hello():
         x = tf.constant([[37.0, -23.0], [1.0, 4.0]])
         w = tf.Variable(tf.random_uniform([2, 2]))
         y = tf.matmul(x, w)
-        output = tf.nn.softmax(y)
         init_op = w.initializer
-
         sess.run(init_op)
 
         result = sess.run(y)
@@ -30,5 +28,20 @@ from flask import Flask, request, jsonify
 @app.route('/api/', methods=['GET', 'POST'])
 def add_message():
     content = request.json
-    print("old speed:", content['speed'])
-    return jsonify({"new_speed":content["speed"] * 2})
+
+    old_speed = content['speed']
+
+    with tf.Session() as sess:
+        x = tf.constant([[old_speed, -23.0], [1.0, 4.0]])
+        w = tf.Variable(tf.random_uniform([2, 2]))
+        y = tf.matmul(x, w)
+        init_op = w.initializer
+        sess.run(init_op)
+
+        result = sess.run(y)
+
+        print("old speed:", old_speed)
+        return jsonify({
+            "new_speed": old_speed * 2,
+            "result": result.__repr__(),
+        })
