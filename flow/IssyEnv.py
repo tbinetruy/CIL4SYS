2cd46bf3501a0175839aad18619cc7af5a552aed
Thomas Binetruy <tbinetruy@gmail.com>
2019-05-02 19:15:12 +0200

    moved invert traffic light state to helpers

--- flow/IssyEnv.py
+++ flow/IssyEnv.py
@@ -98,45 +98,6 @@ class IssyEnvAbstract(Env):
         red_lights = list("ry")
         return [0 if s in red_lights else 1 for s in state]
 
-    def _invert_tl_state(self, id, api="sumo"):
-        """Invert state for given traffic light.
-        It currently only implements conversion for the sumo light state.
-        This function returns the new state string (of the same length as the
-        input state), this allows for handling intersections with different
-        numbers of lanes and lights elegantly.
-
-        This function takes any sumo light state but only convets to a "green"
-        or "red" state. Orange and other states are converted accordingly, see
-        implementation for more detail.
-
-        Parameters
-        ----------
-        id: str
-            ID of traffic light to invert
-            Use `flow.kernel.traffic_light.get_ids` to get a list of traffic
-            light ids.
-        api: str
-            Simulator API which defines the light state format to return.
-            Currently only implements the sumo traffic state format.
-            (see: https://sumo.dlr.de/wiki/Simulation/Traffic_Lights#Signal_state_definitions)
-
-        Returns
-        ----------
-        new_state: str
-            New light state consisting of only red and green lights that
-            oppose the previous state as much as possible.
-
-        """
-        if api == "sumo":
-            old_state = self.k.traffic_light.get_state(id)
-            state = old_state.replace("g", "G")
-            state = state.replace("y", "r")
-            state = state.replace("G", "tmp")
-            state = state.replace("r", "G")
-            state = state.replace("tmp", "r")
-            return state
-        else:
-            return NotImplementedError
 
     def _apply_rl_actions(self, rl_actions):
         """Converts probabilities of switching each lights into actions by
