a663e18eda7f745972a26be818985ab3ab86736c
Thomas Binetruy <tbinetruy@gmail.com>
2019-05-19 23:40:09 +0200

    implemented issyenv3; includes penalize idle in reward

--- flow/IssyEnv.py
+++ flow/IssyEnv.py
@@ -128,3 +128,73 @@ class IssyEnv2(IssyEnv1):
         return self.rewards.penalize_min_speed(
             max_speed) + self.rewards.penalize_max_emission(
                 max_emission) + self.rewards.penalize_tl_switch(tl_penalty)
+
+
+class IssyEnv3(IssyEnv2):
+    """Third environment used to train traffic lights to regulate traffic flow
+    for the Issy les Moulineaux district of study.
+
+    Required from env_params: See parent class
+
+    States
+        An observation is the set of positions, orientations, time spend idled,
+        and speeds of the beta observed vehicles and the binary state of each
+        RL controlled traffic lights.
+
+    Actions
+        See parent class
+
+    Rewards
+        The reward penalizes slow and/or emitting and/or idle time for the beta
+        observable vehicles and traffic light state switches.
+
+    Termination
+        See parent class
+    """
+
+    @property
+    def observation_space(self):
+        """ In this model, we only observe 2D-positions and speed norms of
+        the beta observable vehicles in cartesian coordinates, along with
+        their orientation, absolute speed, CO2 emission and time steps spent
+        idled (speed=0). We also include the binary state of all RL controlled
+        traffic lights.
+
+        Ex: If beta=2 and gamma=10 (2 observed cars and 3 RL controlled
+        traffic lights), our state lives in $R^{6\times2} U B^10$ where
+        B={0,1} is the on/off state each traffic light can take.
+
+        (See parent class for more information)"""
+
+        return Box(
+            low=0,
+            high=float("inf"),
+            shape=(6 * self.scenario.vehicles.num_vehicles +
+                   self.get_num_traffic_lights(), ),
+        )
+
+    def get_state(self, **kwargs):
+        """ We request positions, orientations, speeds, emissions and
+        idled time steps of the beta observable vehicles.
+
+        (See parent class for more information)"""
+        ids = self.get_observable_veh_ids()
+
+        idled = [self.obs_veh_wait_steps[id] for id in ids]
+        idled = pad_list(idled, self.model_params["beta"], 0.)
+
+        return np.concatenate((super().get_state(), idled))
+
+    def compute_reward(self, rl_actions, **kwargs):
+        """ The reward in this simple model is simply the mean velocity
+        of all simulated vehicles present on the mesh devided by the
+        mean CO2 emission.
+
+        (See parent class for more information)"""
+        base_reward = super().compute_reward(rl_actions)
+
+        idled_max_steps = 300
+        idle_reward = self.rewards.penalize_max_wait(self.obs_veh_wait_steps,
+                                                     idled_max_steps, 0, -10)
+
+        return base_reward + idle_reward
