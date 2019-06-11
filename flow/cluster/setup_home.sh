6a807cf126fd1ddc1a04a4891a71b90c573384f8
Thomas Binetruy <tbinetruy@gmail.com>
2019-06-11 15:53:17 +0200

    added setup sumo on school machines

---
+++ flow/cluster/setup_home.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+!pip install --user cmake cython gym==0.12.0 pyprind nose2 cached_property joblib dill lz4 ray==0.6.1 setproctitle psutil opencv-python boto3 redis
+cd ~
+mkdir -p fil_rouge
+cd fil_rouge
+git clone https://github.com/tbinetruy/flow.git
+git clone https://github.com/eclipse/sumo.git && cd sumo && git checkout 1d4338ab80
+git clone https://github.com/tbinetruy/CIL4SYS.git
